f7fb0c3810821ceee4fd41e414534a853f693a63
Corbin Christiansen <corbinpchristiansen@gmail.com>
2026-01-08 10:33:52 -0700

    Implemented basic move logic

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -11,7 +12,12 @@ import java.util.List;
  */
 public class ChessPiece {
 
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -30,14 +36,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -48,6 +54,18 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        ChessPiece piece =  board.getPiece(myPosition);
+
+        if(piece.getPieceType() == PieceType.BISHOP){
+            int startRow = myPosition.getRow();
+            int startCol = myPosition.getColumn();
+
+            List<ChessPosition> endPosition = new ArrayList<>();
+
+            List<ChessMove> possMove = new ArrayList<>();
+
+            return possMove;
+        }
         return List.of();
     }
 }
