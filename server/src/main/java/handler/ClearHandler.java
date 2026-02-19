3d980daf6e83d5438ca42955332ebda5b55e76a3
Corbin Christiansen <corbinpchristiansen@gmail.com>
2026-02-19 07:32:46 -0700

    Created ClearHandler

---
+++ server/src/main/java/handler/ClearHandler.java
@@ -0,0 +1,19 @@
+package handler;
+
+import com.google.gson.Gson;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import org.jetbrains.annotations.NotNull;
+import service.servieImplimentation.ClearService;
+
+import java.util.Map;
+
+public class ClearHandler implements Handler {
+    @Override
+    public void handle(@NotNull Context context){
+        new ClearService().clear();
+        var body = new Gson().toJson(Map.of());
+        context.status(200)
+                .json(body);
+    }
+}
