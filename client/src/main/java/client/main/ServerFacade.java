169770472f18807e8546154601f1d7468dad3a58
Corbin Christiansen <corbinpchristiansen@gmail.com>
2026-03-12 11:45:07 -0600

    Imported starter code. Start of phase 5

---
+++ client/src/main/java/client/main/ServerFacade.java
@@ -0,0 +1,12 @@
+package client.main;
+
+import java.net.http.HttpClient;
+
+public class ServerFacade {
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+}
