ccfc034f8edc4c98fe3700462500d4c69c1b85a9
Corbin Christiansen <corbinpchristiansen@gmail.com>
2026-03-26 12:14:04 -0600

    Created and updated connection maneger

---
+++ server/src/main/java/server/WebSocket/ConnectionManager.java
@@ -0,0 +1,35 @@
+package server.WebSocket;
+
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConnectionManager {
+    public final ConcurrentHashMap<Integer, Set<Session>> connections = new ConcurrentHashMap<>();
+
+    public void add(int gameID, Session session) {
+        connections.computeIfAbsent(gameID, k -> ConcurrentHashMap.newKeySet()).add(session);
+    }
+
+
+    public void remove(int gameID, Session session) {
+        connections.computeIfPresent(gameID, (id, set) -> {
+            set.remove(session);
+            return set.isEmpty() ? null : set;
+        });
+
+    }
+
+    public void broadcast(int gameID, ServerMessage notification) throws IOException {
+        String msg = notification.toString();
+        for (Session c : connections.get(gameID)) {
+            if (c.isOpen()) {
+                c.getRemote().sendString(msg);
+            }
+        }
+    }
+}
